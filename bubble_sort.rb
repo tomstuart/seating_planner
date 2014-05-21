cbe3b961b714b4df34bdfcd651a10c800dd75a89
Tom Stuart <tom@codon.com>
2014-05-22 01:15:21 +0200

    Update bubble sort to always finish

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,17 +4,12 @@ class Array
   end
 
   def bubble_sort!
-    loop do
-      already_sorted = true
-
-      (0...length).each_cons(2) do |i, j|
+    length.downto(1).each do |finish|
+      (0...finish).each_cons(2) do |i, j|
         if yield(self[i], self[j]) > 0
           self[i], self[j] = self[j], self[i]
-          already_sorted = false
         end
       end
-
-      break if already_sorted
     end
 
     self
