b7d434a842c3b92e28d536341f365f5c48b56fae
Tom Stuart <tom@codon.com>
2014-05-22 01:01:50 +0200

    Add initial version of histogram

---
+++ histogram.rb
@@ -0,0 +1,14 @@
+class Hash
+  def to_histogram(width)
+    max_value = values.max
+    max_key_width = keys.map { |key| key.to_s.length }.max
+    max_bar_width = width - max_key_width - max_value.to_s.length - 3
+    scale = max_value.to_f / max_bar_width
+
+    bars = map { |key, value|
+      sprintf "%#{max_key_width}s: %-#{max_bar_width}s %#{max_value.to_s.length}s", key, '#' * (value / scale), value
+    }
+
+    bars.join("\n") + "\n\n" + values.inject(:+).to_s.rjust(width)
+  end
+end
