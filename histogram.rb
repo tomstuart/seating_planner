1cc26afe8ebeadf157d3ac1671ecb822d041150f
Tom Stuart <tom@codon.com>
2014-05-22 01:06:45 +0200

    Calculate size of each value inside #histogram

--- histogram.rb
+++ histogram.rb
@@ -1,11 +1,12 @@
 class Hash
   def to_histogram(width)
+    values = self.values.map(&:size)
     max_value = values.max
     max_key_width = keys.map { |key| key.to_s.length }.max
     max_bar_width = width - max_key_width - max_value.to_s.length - 3
     scale = max_value.to_f / max_bar_width
 
-    bars = map { |key, value|
+    bars = keys.zip(values).map { |key, value|
       sprintf "%#{max_key_width}s: %-#{max_bar_width}s %#{max_value.to_s.length}s", key, '#' * (value / scale), value
     }
 
