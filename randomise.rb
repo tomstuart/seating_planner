b61de297909aa1d406968889d98082b8cd432fc5
Tom Stuart <tom@codon.com>
2014-05-22 17:43:07 +0200

    Implement fair Fisher-Yates shuffle (i.e. Array#shuffle)

--- randomise.rb
+++ randomise.rb
@@ -1,7 +1,14 @@
-require_relative 'bubble_sort'
-
 class Array
   def randomise
-    bubble_sort { [-1, 1].sample }
+    dup.randomise!
+  end
+
+  def randomise!
+    (length - 1).downto(1) do |i|
+      j = 0.upto(i).entries.sample
+      self[i], self[j] = self[j], self[i]
+    end
+
+    self
   end
 end
